dfc3f69cbbbd8d497331ce005996c6bde08b8b8d
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-17 10:07:25 +0300

    add the show tip route and models

--- lib/heads_up_web/controllers/tips_html.ex
+++ lib/heads_up_web/controllers/tips_html.ex
@@ -2,4 +2,13 @@ defmodule HeadsUpWeb.TipsHTML do
   use HeadsUpWeb, :html
 
   embed_templates "tips_html/*"
+
+  def show(assigns) do
+    ~H"""
+    <div class="tips">
+      <h1>You like a Tip, <%= @answer %>?</h1>
+      <p><%= @tip.text %></p>
+    </div>
+    """
+  end
 end
