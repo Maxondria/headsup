d067dd5cecb997b3500a0e256d4ad01da1d820a5
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-17 17:12:09 +0300

    bro, effort live

---
+++ lib/heads_up_web/live/effort_live.ex
@@ -0,0 +1,30 @@
+defmodule HeadsUpWeb.EffortLive do
+  use HeadsUpWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(responders: 0, minutes_per_responder: 10)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="effort">
+      <h1>Community Love</h1>
+      <section>
+        <div>
+          <%= @responders %>
+        </div>
+        *
+        <div>
+          <%= @minutes_per_responder %>
+        </div>
+        =
+        <div>
+          <%= @responders * @minutes_per_responder %>
+        </div>
+      </section>
+    </div>
+    """
+  end
+end
