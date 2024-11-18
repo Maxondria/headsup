da55c34a03cf35b834410bfef93a9c4fa2521e8d
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-18 17:07:58 +0300

    tick - add 3 responders every 2 seconds

--- lib/heads_up_web/live/effort_live.ex
+++ lib/heads_up_web/live/effort_live.ex
@@ -2,6 +2,10 @@ defmodule HeadsUpWeb.EffortLive do
   use HeadsUpWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     {:ok,
      socket
      |> assign(responders: 0, minutes_per_responder: 10)}
@@ -44,5 +48,11 @@ defmodule HeadsUpWeb.EffortLive do
   def handle_event("recalculate", %{"minutes" => minutes}, socket) do
     minutes = String.to_integer(minutes)
     {:noreply, socket |> assign(:minutes_per_responder, minutes)}
+  end
+
+  # Process events
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    {:noreply, socket |> update(:responders, &(&1 + 3))}
   end
 end
