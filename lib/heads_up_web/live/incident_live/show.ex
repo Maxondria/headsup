ca27917c95a5d5c44865281c2ebae0935e037150
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-11-20 17:15:21 +0300

    show an incident

---
+++ lib/heads_up_web/live/incident_live/show.ex
@@ -0,0 +1,39 @@
+defmodule HeadsUpWeb.IncidentLive.Show do
+  use HeadsUpWeb, :live_view
+
+  alias HeadsUp.Incidents
+  import HeadsUpWeb.CustomComponents
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(%{"id" => id}, _uri, socket) do
+    incident = Incidents.get_incident(id)
+    urgent = Incidents.get_urgent(incident)
+
+    {:noreply,
+     socket
+     |> assign(
+       incident: incident,
+       urgent_incidents: urgent,
+       page_title: incident.name
+     )}
+  end
+
+  attr :incidents, :list, required: true
+
+  def urgent_incidents(assigns) do
+    ~H"""
+    <section>
+      <h4>Urgent Incidents</h4>
+      <ul class="incidents">
+        <li :for={incident <- @incidents}>
+          <img src={incident.image_path} />
+          <%= incident.description %>
+        </li>
+      </ul>
+    </section>
+    """
+  end
+end
